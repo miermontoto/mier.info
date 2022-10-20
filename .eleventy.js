1f7e9405f1a0730882596173fb181edaa1cec6ee
miermontoto <mier@mier.info>
2022-10-20 23:30:37 +0200

    restructure, blog entries advancement

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/static/style.css");
+    eleventyConfig.addPassthroughCopy("./src/static/blog.css");
 
     return {
         dir: {
-            input: "src"
+            input: "src",
+            includes: "templates"
         }
     };
 };
