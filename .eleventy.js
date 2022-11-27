3d672a02e002ead549aa11e8f003ef21d8140952
miermontoto <mier@mier.info>
2022-11-27 16:08:24 +0100

    about markdown, render, speed optmztns

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
+const { EleventyRenderPlugin } = require("@11ty/eleventy");
+
 module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyRenderPlugin);
+
     eleventyConfig.addPassthroughCopy("./src/static/**");
+    eleventyConfig.addPassthroughCopy("./src/content/**");
     eleventyConfig.addPassthroughCopy("./assets/**");
 
     return {
---
+++ src/content/about.md
@@ -0,0 +1,6 @@
+- spanish computer engineering undergraduate
+- currently studying at EPI Gijón (University of Oviedo)
+- interested in all things tech and music
+- i like climbing, cycling and playing bass
+- python, java, shell and a little bit of everything else (c/c++, matlab, web, sql, ...)
+- i take notes in markdown using [obsidian](https://obsidian.md).
