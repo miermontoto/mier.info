9397a99fba25d2e3e3b2421a9933b74ec682b4b0
miermontoto <mier@mier.info>
2023-10-30 16:04:28 +0100

    fixes and styling

--- .eleventy.js
+++ .eleventy.js
@@ -7,7 +7,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./assets/**");
 
     eleventyConfig.addShortcode("version", function() {
-        return require("./package.json").version + ' (beta6)';
+        return require("./package.json").version + ' (beta 7)';
     });
 
     eleventyConfig.addShortcode("top", function() {
