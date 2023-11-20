66c0bf8376a17c8a04049ee20f5727c8eb9e98fa
miermontoto <mier@mier.info>
2023-11-20 19:19:08 +0100

    18.0 beta1: background videos, controls, fixes

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,6 +1,8 @@
+let videos;
+let playing = true;
+
 fetch('index.data.json').then(response => response.json()).then(data => {
-    let strings = data.marquee;
-    let string = strings[Math.floor(Math.random() * strings.length)];
+    let string = randomize(data.marquee);
     let marquee = document.getElementById('marquee');
     marquee.innerHTML = string.text;
     if (string.url && string.url !== '') {
@@ -9,6 +11,9 @@ fetch('index.data.json').then(response => response.json()).then(data => {
         });
         marquee.style.cursor = 'help';
     }
+
+    videos = data.videos;
+    injectVideo();
 });
 
 const sections = [
@@ -22,6 +27,25 @@ sections.forEach(({ button, section }) => {
     button.addEventListener('click', () => {
         toggleSection(section);
     });
+});
+
+let fullscreen = document.getElementById('fullscreen-background');
+fullscreen.addEventListener('click', () => {
+    let videoElement = document.getElementById('background');
+
+    if (videoElement.classList.contains('fullscreen')) {
+        videoElement.classList.remove('fullscreen');
+        fullscreen.classList.remove('active')
+    } else {
+        videoElement.classList.add('fullscreen');
+        fullscreen.classList.add('active')
+    }
+});
+
+document.getElementById('stop-background').addEventListener('click', () => {
+    document.getElementById('background').src = '';
+    document.getElementById('overlay').innerHTML = '';
+    document.getElementById('background').style.zIndex = '-1';
 });
 
 // order projects by starred
@@ -54,4 +78,19 @@ function toggleSection(section) {
     if (!sectionIsActive) {
         section.classList.add('active');
     }
+}
+
+function injectVideo() {
+    let video = randomize(videos);
+    let videoElement = document.getElementById('background');
+
+    let source = document.createElement('source');
+    source.src = `/assets/media/${video}`;
+    source.type = 'video/mp4';
+    videoElement.appendChild(source);
+    videoElement.play();
+}
+
+function randomize(col) {
+    return col[Math.floor(Math.random() * col.length)];
 }
