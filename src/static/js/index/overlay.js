3e8bdc54a0e4240b8cb7a0567d73376ade3c2481
miermontoto <mier@mier.info>
2023-12-10 19:43:30 +0100

    19.0 RC2 [index] - background videos:   - compressed and encoded background videos using av1   - added title to videos in data file   - chained videos on end - overlay:   - better styling   - new 'next' button   - show 'now playing' on video change   - load overlay from js instead of html - better randomize lib
    [general]
    - minor tweaks

--- src/static/js/index/overlay.js
+++ src/static/js/index/overlay.js
@@ -1,22 +1,61 @@
-let fullscreen = document.getElementById('fullscreen-background');
-let overlay = document.getElementById('overlay');
+import {load} from './video.js';
+let videoElement = document.getElementById('background');
 
+// create elements
+let overlay_container = document.createElement('div');
+let overlay_box = document.createElement('div');
+let fullscreen = document.createElement('span');
+let stop = document.createElement('span');
+let next = document.createElement('span');
+let nowplaying = document.createElement('span');
+
+// set attributes
+overlay_container.setAttribute('id', 'overlay-container');
+overlay_box.setAttribute('id', 'overlay-box');
+fullscreen.setAttribute('id', 'bg-fullscreen');
+stop.setAttribute('id', 'bg-stop');
+next.setAttribute('id', 'bg-next');
+nowplaying.setAttribute('id', 'bg-nowplaying');
+
+// set innerHTML
+fullscreen.innerHTML = '⛶';
+stop.innerHTML = '⏹';
+next.innerHTML = '⏭';
+
+// set classes
+[fullscreen, stop, next].forEach(element => {
+    element.classList.add('button');
+    element.classList.add('light');
+});
+
+// add functionality
 fullscreen.addEventListener('click', () => {
-    let videoElement = document.getElementById('background');
-
     if (videoElement.classList.contains('fullscreen')) {
         videoElement.classList.remove('fullscreen');
         fullscreen.classList.remove('active');
-        overlay.style.zIndex = '2';
+        overlay_box.classList.remove('active');
+        overlay_container.style.zIndex = '2';
     } else {
         videoElement.classList.add('fullscreen');
         fullscreen.classList.add('active');
-        overlay.style.zIndex = '4';
+        overlay_box.classList.add('active');
+        overlay_container.style.zIndex = '4';
     }
 });
 
-document.getElementById('stop-background').addEventListener('click', () => {
-    document.getElementById('background').src = '';
-    document.getElementById('overlay').innerHTML = '';
-    document.getElementById('background').style.zIndex = '-1';
+stop.addEventListener('click', () => {
+    videoElement.remove();
+    document.getElementById('overlay').remove();
 });
+
+next.addEventListener('click', () => {
+    load();
+});
+
+// append elements
+overlay_box.appendChild(fullscreen);
+overlay_box.appendChild(stop);
+overlay_box.appendChild(next);
+overlay_container.appendChild(overlay_box);
+overlay_container.appendChild(nowplaying);
+document.body.appendChild(overlay_container);
--- src/static/js/index/randomize.js
+++ src/static/js/index/randomize.js
@@ -1,3 +1,11 @@
-export default function randomize(col) {
-    return col[Math.floor(Math.random() * col.length)];
+export function randElement(col) {
+    return col.at([Math.floor(Math.random() * col.length)]);
+}
+
+export function randArray(col) {
+    for (let i = col.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [col[i], col[j]] = [col[j], col[i]];
+    }
+    return col;
 }
