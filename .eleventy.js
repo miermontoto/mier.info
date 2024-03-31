c9f3547eb2a926283332585a5a2c24494b33e0e9
Juan Mier <mier@mier.info>
2024-03-31 03:41:36 +0200

    21.4 [general] - add pnpm lock - minor fixes
    [changelog]
    - fix live commit showing out of order
    - fix oneline commits not showing as titles
    - manually build changelog before deploy
    - fix missing commits
    
    [projects]
    - fix minor styling issues

--- .eleventy.js
+++ .eleventy.js
@@ -101,12 +101,17 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addShortcode("changelog", function(data) {
         let content = '<div id="changelog">'
 
+        // sort commits by version
+        data.sort((a, b) => {
+            return a.version.major > b.version.major ? -1 : 1;
+        })
+
         // if the current version isn't in the changelog, add it manually to the data array
         let currentJson = require('./package.json')
         let currentVersion = currentJson.version.split('.')
         let currentChannel = currentJson.channel ? ` ${currentJson.channel}` : ''
         if (!data.find(d => d.version.major == currentVersion[1] && d.version.minor == currentVersion[2])) {
-            data.push({
+            data.unshift({
                 "version": {
                     "major": currentVersion[1],
                     "minor": currentVersion[2]
@@ -114,22 +119,20 @@ module.exports = function (eleventyConfig) {
                 "title": `${currentVersion[1]}.${currentVersion[2]}${currentChannel}`,
                 "date": "ongoing",
                 "message": "<span class='warning'>changelog for this patch will be built in the next version</span>",
-                "hash": null
+                "hash": "main"
             })
         }
 
-        // sort commits by version
-        data.sort((a, b) => {
-            return a.version.major > b.version.major ? -1 : 1;
-        })
-
         let prevMajor = null
         data.forEach((d) => {
+            // if the major version has changed, add a new header
             if (prevMajor != d.version.major) {
                 if (prevMajor) content += '<hr>'
                 content += `<h1>v${d.version.major}</h1>`
             }
             prevMajor = d.version.major
+
+            // add the commit entry
             content += `<div class="entry hoverborder">
                 <span class="title">${d.title}</span>`
             if (d.message) content += `<br><span class="message">${d.message.replace(/\n/g, '<br>')}</span>`
